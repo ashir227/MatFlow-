5f41c82a68d28e3cf25739b8f59957ffe2f61d74
Ashir Ahmed <ashirahmed810@gmail.com>
2026-03-05 18:22:53 +0500

    Splash

---
+++ matflow/lib/Screens/Splash.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:matflow/Screens/login.dart';
+
+class SplashScr extends StatefulWidget {
+  const SplashScr({super.key});
+
+  @override
+  State<SplashScr> createState() => _SplashScrState();
+}
+
+class _SplashScrState extends State<SplashScr> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScr()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ matflow/lib/Screens/login.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginScr extends StatelessWidget {
+  const LoginScr({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
